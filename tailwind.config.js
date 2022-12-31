efb78b2f7d617a6e1dca699439bff0d8b87cc868
Denis-D-S <denisduartedasilva@hotmail.com>
2022-12-31 19:37:53 -0300

    começando testes em tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+//
+// content: [
+//   './pages/**/*.{html,js}',
+//   './components/**/*.{html,js}',
+//   './components/**/*.jsx',
+//   './components/**/*.tsx',
+//   "./src/**/*.{html,js}",
+//   "./src/**/*.{html,jsx}",
+//   "./src/**/*.{html,js}",
+//   '*.{html,js}',
+//   "./src/**/*.{html,jsx}",],

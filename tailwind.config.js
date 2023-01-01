73facdb6247a9b8df7497b7021b51a1ea5117020
Denis-D-S <denisduartedasilva@hotmail.com>
2023-01-01 11:50:35 -0300

    tailwind funcionando

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
+    "./src/**/*.{html,ts}", //é necessário especificar o caminho para a pasta que você quer que o Tailwind possa ler
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-//
-// content: [
-//   './pages/**/*.{html,js}',
-//   './components/**/*.{html,js}',
-//   './components/**/*.jsx',
-//   './components/**/*.tsx',
-//   "./src/**/*.{html,js}",
-//   "./src/**/*.{html,jsx}",
-//   "./src/**/*.{html,js}",
-//   '*.{html,js}',
-//   "./src/**/*.{html,jsx}",],
+
